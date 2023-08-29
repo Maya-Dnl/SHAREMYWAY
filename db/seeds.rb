dae0b74985a169a2af4d24463b7faeb06f20b004
ferit zengin <feritzengin72300@icloud.com>
2023-08-29 16:01:46 +0200

    en pause

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,95 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "cleaning data base"
+Booking.destroy_all
+Event.destroy_all
+User.destroy_all
+
+puts "starting seed"
+# ["music", "trip", "fashion", "mathematics", "coffee", "movies"]
+user1 = User.create!(
+  email: "beatrice-routine@icloud.com",
+  password: "123456",
+  username: "Bea Routine",
+  language: "French",
+  hobby1: "coffee",
+  hobby2: "music",
+  hobby3: "movies"
+)
+
+user2 = User.create!(
+  email: "steven-richard@outlook.com",
+  password: "123456",
+  username: "Steven Richar44",
+  language: "French",
+  hobby1: "music",
+  hobby2: "trip",
+  hobby3: "fashion"
+)
+
+user3 = User.create!(
+  email: "john.marchand@gmail.com",
+  password: "123456",
+  username: "John Marchand",
+  language: "French",
+  hobby1: "trip",
+  hobby2: "music",
+  hobby3: ""
+)
+
+event1 = Event.create!(
+  name: "The great trip",
+  address: "The saint michel mont",
+  date: "02/09/2024",
+  description: "Come with us for the best holidays of your life",
+  user: user2
+)
+
+
+file = File.open("app/assets/images/visiting-mont-saint-michel-france-tips.jpeg")
+event1.photo.attach(io: file, filename: "mont-saint-michel.jpg", content_type: "image/jpeg")
+event1.save
+
+
+event2 = Event.create!(
+  name: "Movies party",
+  address: "09 avenue de la poule noire, Nantes",
+  date: "02/09/2023",
+  description: "Come with us for the best movies party of your life",
+  user: user3
+)
+
+file = File.open("app/assets/images/movie1.jpg")
+event2.photo.attach(io: file, filename: "camping.jpg", content_type: "image/jpg")
+event2.save
+
+event3 = Event.create!(
+  name: "Look fashion style",
+  address: "04 rue des beaux styles, strasbourg",
+  date: "02/08/2023",
+  description: "Fashion style is in your bear",
+  user: user1
+)
+
+event4 = Event.create!(
+  name: "Mathematic with us",
+  address: "1 rue des mathematicien",
+  date: "02/11/2023",
+  description: "With us become more stronger in mathematics",
+  user: user2
+)
+
+event5 = Event.create!(
+  name: "Dance witch us for all night",
+  address: "7 rue des danseur, paris",
+  date: "24/11/2023",
+  description: "Danse all night and discover paris-night life",
+  user: user3
+)
+
+# Create a booking associated with the user
+booking1 = Booking.create!(
+  user: user2,
+  event: event1
+)
