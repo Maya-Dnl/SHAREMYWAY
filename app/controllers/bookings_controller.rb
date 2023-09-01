a180535efd26c8e96e5015dbc6524f3218ac9b84
maya dnl <daniel.maywen78@gmail.com>
2023-09-01 15:00:39 +0200

    button join event redirect to my bookings ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,41 @@
 class BookingsController < ApplicationController
 
+  before_action :set_booking, only: [:show]
+
   def index
     @bookings = Booking.all
   end
 
-  def create
+  def new
+    @booking = Booking.new
+    @event = Event.find(params[:event_id])
   end
 
+  def create
+    @event = Event.find(params[:event_id])
+    @booking = Booking.new
+    @booking.user = current_user
+    @booking.event = @event
+    if @booking.save!
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    # We recuperate our bookings with their IDs
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
 
-  before_action :set_event, only: [:show]
+  before_action :set_event, only: [:show, :join]
 
   def index
     @events = Event.all
