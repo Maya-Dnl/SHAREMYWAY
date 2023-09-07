30b2ac42eb1340fa51cb90621991fc20277d3098
maya dnl <daniel.maywen78@gmail.com>
2023-09-07 17:41:22 +0200

    test bug heroku

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,10 +5,11 @@ class RegistrationsController < Devise::RegistrationsController
 
   def configure_account_update_params
     # Permettre la mise à jour du profil sans le mot de passe actuel
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :username, :language, :hobby1, :hobby2, :hobby3])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :username, :language])
   end
 
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
 end
+# , :hobby1, :hobby2, :hobby3
